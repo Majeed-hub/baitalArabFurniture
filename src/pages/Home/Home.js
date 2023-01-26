6f3b73acca45ab4c153b603e82756a53584e5628
Majeed-hub <majeedbkl65@gmail.com>
2023-01-26 15:36:24 +0530

    updated home page again [majeed]

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,7 +2,12 @@ import React from 'react';
 import './Home.css';
 
 const Home = () => {
-  return <h1>Updated Home page</h1>;
+  return (
+    <>
+      <h1>Updated Home page</h1>
+      <h1>Updated Home page again [majeed]</h1>
+    </>
+  );
 };
 
 export default Home;
