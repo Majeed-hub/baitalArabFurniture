caf378677a6337cfb5cbe6b2d64cb01d4f2beefe
Ruvaim <ruvaim51@gmail.com>
2023-01-28 08:19:17 +0530

    completed portfolio page

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -1,8 +1,72 @@
 import React from 'react';
+import Portfolio1 from '../../Assests/images/portfolio1.png';
 import './Portfolio.css';
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return (
+    <div className="portfolioContainer">
+      <div className="portfolioTitle">
+        <h2>Portfolio</h2>
+      </div>
+      <div className="portfolioCardWrapper">
+        <div class="portfolioCard">
+          <img src={Portfolio1} alt="" />
+          <div class="overlay">
+            <div className="portfolioCardHover">
+              <p>Decorative Table</p>
+              <span>Interior</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="portfolioCard">
+          <img src={Portfolio1} alt="" />
+          <div className="overlay">
+            <div className="portfolioCardHover">
+              <p>Decorative Table</p>
+              <span>Interior</span>
+            </div>
+          </div>
+        </div>
+        <div className="portfolioCard">
+          <img src={Portfolio1} alt="" />
+          <div className="overlay">
+            <div className="portfolioCardHover">
+              <p>Decorative Table</p>
+              <span>Interior</span>
+            </div>
+          </div>
+        </div>
+        <div className="portfolioCard">
+          <img src={Portfolio1} alt="" />
+          <div className="overlay">
+            <div className="portfolioCardHover">
+              <p>Decorative Table</p>
+              <span>Interior</span>
+            </div>
+          </div>
+        </div>
+        <div className="portfolioCard">
+          <img src={Portfolio1} alt="" />
+          <div className="overlay">
+            <div className="portfolioCardHover">
+              <p>Decorative Table</p>
+              <span>Interior</span>
+            </div>
+          </div>
+        </div>
+        <div className="portfolioCard">
+          <img src={Portfolio1} alt="" />
+          <div className="overlay">
+            <div className="portfolioCardHover">
+              <p>Decorative Table</p>
+              <span>Interior</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
