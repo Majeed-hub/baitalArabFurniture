77506c988c98085f7963be37deb186ba7938cda4
Ruvaim <ruvaim51@gmail.com>
2023-01-30 00:56:47 +0530

    fixed footer extra space bug and minimize the width of navbar icon

--- src/utils/Navbar/Navbar.js
+++ src/utils/Navbar/Navbar.js
@@ -25,11 +25,11 @@ const Navbar = () => {
           }}
         >
           {isNavExpanded ? (
-            <svg xmlns="http://www.w3.org/2000/svg" height="38" width="38">
+            <svg xmlns="http://www.w3.org/2000/svg" height="36" width="36">
               <path d="m12.45 37.65-2.1-2.1L21.9 24 10.35 12.45l2.1-2.1L24 21.9l11.55-11.55 2.1 2.1L26.1 24l11.55 11.55-2.1 2.1L24 26.1Z" />
             </svg>
           ) : (
-            <svg xmlns="http://www.w3.org/2000/svg" height="38" width="38">
+            <svg xmlns="http://www.w3.org/2000/svg" height="37" width="30">
               <path d="M6 36v-3h36v3Zm0-10.5v-3h36v3ZM6 15v-3h36v3Z" />
             </svg>
           )}
