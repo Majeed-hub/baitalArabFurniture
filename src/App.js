b33a41a297aaf045c6df723f3f94eb39ef45491e
Majeed-hub <majeedbkl65@gmail.com>
2023-01-31 00:47:35 +0530

    solved scroll issue by creating a scrollToTop function in utils folder

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter } from 'react-router-dom';
 import Navbar from './utils/Navbar/Navbar';
 import AllRoutes from './AllRoutes';
 import Footer from './utils/Footer/Footer';
+import ScrollToTop from './utils/scroll_to_top/scrollToTop';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+      <ScrollToTop/>
         <Navbar />
         <AllRoutes />
         <Footer />
