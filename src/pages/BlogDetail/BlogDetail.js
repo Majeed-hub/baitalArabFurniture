1fc56debd5a84c4f10e2a2b38c47d633867fb495
Majeed-hub <majeedbkl65@gmail.com>
2023-01-30 12:59:26 +0530

    created go back button

--- src/pages/BlogDetail/BlogDetail.js
+++ src/pages/BlogDetail/BlogDetail.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { Container, ArticleContent, Heading } from "./BlogDetail.elements";
 import { Article1, Article2, Article3, Article4 } from "./Data/BlogData";
+import "./BlogDetail.css";
 
 const BlogDetail = () => {
   const { id } = useParams();
@@ -11,24 +12,36 @@ const BlogDetail = () => {
         <Container>
           <Heading>{Article1.title}</Heading>
           <ArticleContent>{Article1.content}</ArticleContent>
+          <Link className="btn btnGoBack" to={`/blog`}>
+            Go Back
+          </Link>
         </Container>
       )}
       {id === "2" && (
         <Container>
           <Heading>{Article2.title}</Heading>
           <ArticleContent>{Article2.content}</ArticleContent>
+          <Link className="btn btnGoBack" to={`/blog`}>
+            Go Back
+          </Link>
         </Container>
       )}
       {id === "3" && (
         <Container>
           <Heading>{Article3.title}</Heading>
           <ArticleContent>{Article3.content}</ArticleContent>
+          <Link className="btn btnGoBack" to={`/blog`}>
+            Go Back
+          </Link>
         </Container>
       )}
       {id === "4" && (
         <Container>
           <Heading>{Article4.title}</Heading>
           <ArticleContent>{Article4.content}</ArticleContent>
+          <Link className="btn btnGoBack" to={`/blog`}>
+            Go Back
+          </Link>
         </Container>
       )}
     </>
