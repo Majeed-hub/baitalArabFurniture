265a9586061ed5f9e4ccd633f53e8f8c7bd8b583
Ruvaim <ruvaim51@gmail.com>
2023-01-31 00:22:40 +0530

    updated home page and added our services and get to know us in home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Home.css';
+// import '../AboutUs/AboutUs.css';
 import Banner from '../../Assests/images/DesktopHeroSection.png';
 import Banner_2 from '../../Assests/images/LivingRoom.jpg';
 import Banner_3 from '../../Assests/images/Modern_Wardrobe.jpg';
@@ -9,10 +10,19 @@ import Sofa1 from '../../Assests/images/sofa1.png';
 import Bed1 from '../../Assests/images/bed.png';
 import Table1 from '../../Assests/images/table.png';
 import Cart from '../../components/HomeCard/Card';
+import ProductCard from '../../components/ProductCard/ProductCard';
+import TestimonialCard from '../../components/TestimonialCard/TestimonialCard';
+import AboutUsCard from '../../components/AboutUsCard/AboutUsCard';
+
+import Leadership from '../../Assests/images/leadership.png';
+import Consultation from '../../Assests/images/consultation.svg';
+import Idea from '../../Assests/images/idea.svg';
+import MaterialSelection from '../../Assests/images/choice.svg';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div className="homePage">
+    <div>
       <div
         id="carouselExampleAutoplaying"
         className="carousel slide"
@@ -69,7 +79,18 @@ const Home = () => {
           <div className="productTitle">
             <h2>Products</h2>
           </div>
-          <div className="itemTitle">
+          <div className="itemCardContainer">
+            <ProductCard
+              title={'Sofa'}
+              content={
+                'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosaanimi explicabo, rem possimus eum vitae minima voluptatibus iusto etea quam quas, cum ut quos officia delectus,Lorem ipsum dolor sit amet,consectetur adipisicing elit. Eaque veniam eos nam.'
+              }
+            />
+            <Cart img={Sofa1} title={'Single Seater'} />
+            <Cart img={Sofa1} title={'Single Seater'} />
+          </div>
+
+          {/* <div className="itemTitle">
             <h3>Sofa</h3>
           </div>
 
@@ -77,9 +98,19 @@ const Home = () => {
             <Cart img={Sofa1} title={'Single Seater'} price={'500 aed'} />
             <Cart img={Sofa1} title={'Single Seater'} price={'500 aed'} />
             <Cart img={Sofa1} title={'Single Seater'} price={'500 aed'} />
-          </div>
+          </div> */}
         </div>
-        <div className="itemContainer">
+        <div className="itemCardContainer">
+          <ProductCard
+            title={'Bed'}
+            content={
+              'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosaanimi explicabo, rem possimus eum vitae minima voluptatibus iusto etea quam quas, cum ut quos officia delectus,Lorem ipsum dolor sit amet,consectetur adipisicing elit. Eaque veniam eos nam.'
+            }
+          />
+          <Cart img={Bed1} title={'Master bed'} />
+          <Cart img={Bed1} title={'Master bed'} />
+        </div>
+        {/* <div className="itemContainer">
           <div className="itemTitle">
             <h3>Bed</h3>
           </div>
@@ -89,8 +120,8 @@ const Home = () => {
             <Cart img={Bed1} title={'Master bed'} price={'500 aed'} />
             <Cart img={Bed1} title={'Master bed'} price={'500 aed'} />
           </div>
-        </div>
-        <div className="itemContainer">
+        </div> */}
+        {/* <div className="itemContainer">
           <div className="itemTitle">
             <h3>Table</h3>
           </div>
@@ -100,6 +131,100 @@ const Home = () => {
             <Cart img={Table1} title={'Dinning Table'} price={'500 aed'} />
             <Cart img={Table1} title={'Dinning Table'} price={'500 aed'} />
           </div>
+        </div> */}
+        <div className="itemCardContainer">
+          <ProductCard
+            title={'Table'}
+            content={
+              'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosaanimi explicabo, rem possimus eum vitae minima voluptatibus iusto etea quam quas, cum ut quos officia delectus,Lorem ipsum dolor sit amet,consectetur adipisicing elit. Eaque veniam eos nam.'
+            }
+          />
+          <Cart img={Table1} title={'Dinning Table'} />
+          <Cart img={Table1} title={'Dinning Table'} />
+        </div>
+        <div className="itemCardContainer">
+          <ProductCard
+            title={'Wardrobe/Cabinet'}
+            content={
+              'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosaanimi explicabo, rem possimus eum vitae minima voluptatibus iusto etea quam quas, cum ut quos officia delectus,Lorem ipsum dolor sit amet,consectetur adipisicing elit. Eaque veniam eos nam.'
+            }
+          />
+          <Cart img={Table1} title={'Dinning Table'} />
+          <Cart img={Table1} title={'Dinning Table'} />
+        </div>
+      </div>
+      <div className="aboutUsContainer">
+        <div className="aboutUsTestimonial">
+          <div className="aboutUsTitle">
+            <h2>Get To Know Us</h2>
+          </div>
+          <div className="aboutUsCardWrapper">
+            <TestimonialCard
+              image={Leadership}
+              title={'LEADERSHIP'}
+              desc={
+                'Out multicultural leaders have deep start-up and enterprise experience. with rich mobile and data expertise.'
+              }
+            />
+            <TestimonialCard
+              image={Leadership}
+              title={'CAREERS'}
+              desc={
+                'We take great pride in out market leadership position. thanks to the hard work of out incredibly talented.'
+              }
+            />
+            <TestimonialCard
+              image={Leadership}
+              title={'PARTNERSHIPS'}
+              desc={
+                'From research, to events, to platform partnerships. we work with thought leaders to prodive more actionable data.'
+              }
+            />
+            <TestimonialCard
+              image={Leadership}
+              title={'PRESS'}
+              desc={
+                "We are the forefront of the market's conversation and serve as the most trusted global source."
+              }
+            />
+          </div>
+        </div>
+        <div className="aboutUsOurServices">
+          <div className="aboutUsTitle">
+            <h2>Our Services</h2>
+          </div>
+          <div className="aboutUsCardWrapper">
+            <AboutUsCard
+              img={Consultation}
+              title={'Consultation'}
+              desc={'Determine the style and material of the interior.'}
+            />
+            <AboutUsCard
+              img={Idea}
+              title={'Idea'}
+              desc={'We provide innovative ideas.'}
+            />
+            <AboutUsCard
+              img={MaterialSelection}
+              title={'Material Selection'}
+              desc={'Selection of material to be used.'}
+            />
+          </div>
+        </div>
+        <div className="aboutUsTitle">
+          <h2>About Us</h2>
+        </div>
+        <div className="aboutUsDescription">
+          <p>
+            Furniture has been an integral part of human life for a very long
+            time, and one cannot simply imagine his/her life without its
+            presence. It needs to be a memorable and enjoyable experience and
+            can be possible only when it is handcrafted to a great extent by
+            skilled and experienced craftsmen.{' '}
+            <Link style={{ textDecoration: 'none' }} to={'/aboutus'}>
+              Read more
+            </Link>
+          </p>
         </div>
       </div>
     </div>
