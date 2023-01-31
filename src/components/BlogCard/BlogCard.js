43afe7439bb5b10e9db18fdcc50781870aa025ff
Majeed-hub <majeedbkl65@gmail.com>
2023-01-31 19:30:45 +0530

    fixed aligment and font size of blog card

--- src/components/BlogCard/BlogCard.js
+++ src/components/BlogCard/BlogCard.js
@@ -7,16 +7,16 @@ export const BlogCard = ({ id, title, content }) => {
     <div
       class="card"
       style={{
-        width: '18rem',
+        width: '20rem',
         backgroundColor: '#FAF8F1',
-        height: '250px',
+        height: '290px',
         display: 'inline-block',
         margin: '7px',
       }}
     >
       <div class="card-body">
         <h5 class="card-title">{title}</h5>
-        <p class="card-text">{content}</p>
+        <p class="card-text" style={{fontSize:"25px"}}>{content}</p>
         <Link className="btn btnViewMore" to={`/blog-detail/${id}`}>
           Read More
         </Link>
