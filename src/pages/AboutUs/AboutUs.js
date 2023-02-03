558e7899a7f2db6de6a1392f216aef45662a99b8
Ruvaim <ruvaim51@gmail.com>
2023-02-03 17:24:39 +0530

    Removed Our team section from about us page

--- src/pages/AboutUs/AboutUs.js
+++ src/pages/AboutUs/AboutUs.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import './AboutUs.css';
 import AboutUsBanner from '../../Assests/images/LivingRoom.jpg';
-import AboutUsCard from '../../components/AboutUsCard/AboutUsCard';
-import ProfilePhoto from '../../Assests/images/sofa1.png';
-// import Leadership from '../../Assests/images/leadership.png';
-// import Consultation from '../../Assests/images/consultation.svg';
-// import Idea from '../../Assests/images/idea.svg';
-// import MaterialSelection from '../../Assests/images/choice.svg';
-// import TestimonialCard from '../../components/TestimonialCard/TestimonialCard';
+// import AboutUsCard from '../../components/AboutUsCard/AboutUsCard';
+// import ProfilePhoto from '../../Assests/images/sofa1.png';
 
 const AboutUs = () => {
   return (
@@ -55,7 +50,7 @@ const AboutUs = () => {
           <br />
         </p>
       </div>
-      <div className="aboutUsOurTeam">
+      {/* <div className="aboutUsOurTeam">
         <div className="aboutUsTitle">
           <h1>Our Team</h1>
         </div>
@@ -74,63 +69,6 @@ const AboutUs = () => {
             img={ProfilePhoto}
             title={'Abdul Majeed'}
             desc={'Designer'}
-          />
-        </div>
-      </div>
-      {/* <div className="aboutUsTestimonial">
-        <div className="aboutUsTitle">
-          <h1>Get To Know Us</h1>
-        </div>
-        <div className="aboutUsCardWrapper">
-          <TestimonialCard
-            image={Leadership}
-            title={'LEADERSHIP'}
-            desc={
-              'Out multicultural leaders have deep start-up and enterprise experience. with rich mobile and data expertise.'
-            }
-          />
-          <TestimonialCard
-            image={Leadership}
-            title={'CAREERS'}
-            desc={
-              'We take great pride in out market leadership position. thanks to the hard work of out incredibly talented.'
-            }
-          />
-          <TestimonialCard
-            image={Leadership}
-            title={'PARTNERSHIPS'}
-            desc={
-              'From research, to events, to platform partnerships. we work with thought leaders to prodive more actionable data.'
-            }
-          />
-          <TestimonialCard
-            image={Leadership}
-            title={'PRESS'}
-            desc={
-              "We are the forefront of the market's conversation and serve as the most trusted global source."
-            }
-          />
-        </div>
-      </div>
-      <div className="aboutUsOurServices">
-        <div className="aboutUsTitle">
-          <h1>Our Services</h1>
-        </div>
-        <div className="aboutUsCardWrapper">
-          <AboutUsCard
-            img={Consultation}
-            title={'Consultation'}
-            desc={'Determine the style and material of the interior.'}
-          />
-          <AboutUsCard
-            img={Idea}
-            title={'Idea'}
-            desc={'We provide innovative ideas.'}
-          />
-          <AboutUsCard
-            img={MaterialSelection}
-            title={'Material Selection'}
-            desc={'Selection of material to be used.'}
           />
         </div>
       </div> */}
