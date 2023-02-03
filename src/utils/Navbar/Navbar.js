d0a7c275edff1f43559ef5171665b7d71ebb5646
Majeed-hub <majeedbkl65@gmail.com>
2023-02-03 11:55:14 +0530

    removed useRef from navbar.js

--- src/utils/Navbar/Navbar.js
+++ src/utils/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState, useRef } from 'react';
+import { useState} from 'react';
 import './Navbar.css';
 import { Link, NavLink } from 'react-router-dom';
 import {HashLink} from 'react-router-hash-link';
