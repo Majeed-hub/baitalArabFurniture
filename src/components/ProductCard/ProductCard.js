761fcda8c90e528225847c29c43e8c35d0f6ae89
Majeed-hub <majeedbkl65@gmail.com>
2023-02-10 20:20:32 +0530

    post production update 2

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -5,10 +5,10 @@ import './ProductCard.css';
 
 export const ProductCard = ({ title, content }) => {
   return (
-    <div class="productCard">
-      <div class="productCardBody">
-        <h3 class="productCardTitle">{title}</h3>
-        <p class="productCardText">{content}</p>
+    <div className="productCard">
+      <div className="productCardBody">
+        <h3 className="productCardTitle">{title}</h3>
+        <p className="productCardText">{content}</p>
         <Link className="btn btnViewMore" to={`/product`}>
           View More
         </Link>
